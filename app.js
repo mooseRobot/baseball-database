3b0e90e86b886e6ec2888a8d675b0bee3cf2c5ba
FelixGerard Pidlaoan <pidlaoaf@flip2.engr.oregonstate.edu>
2023-07-27 22:14:38 -0700

    updated website

---
+++ app.js
@@ -0,0 +1,145 @@
+// App.js
+
+/*
+    SETUP
+*/
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+PORT        = 10503;                 // Set a port number at the top so it's easy to change in the future
+
+
+// app.js
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+// app.js
+
+// Database
+var db = require('./database/db-connector')
+
+/*
+    ROUTES
+*/
+// app.js
+
+
+    app.get('/', function(req, res)
+    {  
+        let query1 = "SELECT * FROM teams;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query
+
+
+
+    app.post('/add-team-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+    
+        // Capture NULL values
+        let gameswon = parseInt(data.gameswon);
+        if (isNaN(gameswon))
+        {
+            gameswon = 'NULL'
+        }
+    
+        let gameslost = parseInt(data.gameslost);
+        if (isNaN(gameslost))
+        {
+            gameslost = 'NULL'
+        }
+    
+        // Create the query and run it on the database
+        query1 = `INSERT INTO teams (teamname, location, gameswon, gameslost) VALUES ('${data.teamname}', '${data.location}', ${gameswon}, ${gameslost})`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on bsg_people
+                query2 = `SELECT * FROM teams;`;
+                db.pool.query(query2, function(error, rows, fields){
+    
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        res.send(rows);
+                    }
+                })
+            }
+        })
+    });
+
+
+    app.post('/add-team-form', function(req, res){
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+    
+        // Capture NULL values
+        let gameswon = parseInt(data['input-gameswon']);
+        if (isNaN(gameswon))
+        {
+            gameswon = 'NULL'
+        }
+    
+        let gameslost = parseInt(data['input-gameslost']);
+        if (isNaN(gameslost))
+        {
+            gameslost = 'NULL'
+        }
+    
+        // Create the query and run it on the database
+        query1 = `INSERT INTO teams (teamname, location, gameswon, gameslost) VALUES ('${data['input-teamname']}', '${data['input-location']}', '${gameswon}',' ${gameslost}')`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+    
+            // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+            // presents it on the screen
+            else
+            {
+                res.redirect('/');
+            }
+        })
+    })
+
+
+
+
+
+
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
---
+++ public/js/add_team.js
@@ -0,0 +1,94 @@
+// Get the objects we need to modify
+let addTeamForm = document.getElementById('add-team-form-ajax');
+
+// Modify the objects we need
+addTeamForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputTeamname = document.getElementById("input-teamname");
+    let inputLocation = document.getElementById("input-location");
+    let inputGamesWon = document.getElementById("input-gameswon");
+    let inputGamesLost = document.getElementById("input-gameslost");
+
+    // Get the values from the form fields
+    let teamnameValue = inputTeamname.value;
+    let locationValue = inputLocation.value;
+    let gamesWonValue = inputGamesWon.value;
+    let gamesLostValue = inputGamesLost.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        teamname: teamnameValue,
+        location: locationValue,
+        gameswon: gamesWonValue,
+        gameslost: gamesLostValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-team-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputTeamname.value = '';
+            inputLocation.value = '';
+            inputGamesWon.value = '';
+            inputGamesLost.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// bsg_people
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("teams-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let teamnameCell = document.createElement("TD");
+    let locationCell = document.createElement("TD");
+    let gamesWonCell = document.createElement("TD");
+    let gamesLostCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    teamnameCell.innerText = newRow.teamname;
+    locationCell.innerText = newRow.location;
+    gamesWonCell.innerText = newRow.gameswon;
+    gamesLostCell.innerText = newRow.gameslost;
+
+    // Add the cells to the row 
+    row.appendChild(teamnameCell);
+    row.appendChild(locationCell);
+    row.appendChild(gamesWonCell);
+    row.appendChild(gamesLostCell);
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
