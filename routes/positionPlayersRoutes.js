961f400c3b4a8e3e6b04ba18af94ad5af3a1cb28
Felix <fpidlaoan@gmail.com>
2023-08-07 00:59:23 -1000

    Added position players table and get routes to position players

--- routes/positionPlayersRoutes.js
+++ routes/positionPlayersRoutes.js
@@ -3,7 +3,36 @@ const router = express.Router();
 const db = require('../database/db-connector');
 
 router.get('/', function(req, res) {
-    return res.render('positionPlayers')
-})
+    let query1;
+
+    if (req.query.playername === undefined)
+    {
+        query1 = `
+                SELECT
+                    position_players.players_playername AS player_name,
+                    position_players.position,
+                    position_players.battingaverage AS batting_average,
+                    position_players.homeruns, position_players.rbi
+                FROM position_players
+                INNER JOIN players ON position_players.players_playername = players.playername;`;
+    } else {
+        query1 = `
+                SELECT
+                    position_players.players_playername AS player_name,
+                    position_players.position,
+                    position_players.battingaverage AS batting_average,
+                    position_players.homeruns, position_players.rbi
+                FROM position_players
+                INNER JOIN players ON position_players.players_playername = players.playername
+                WHERE position_players.players_playername LIKE "${req.query.playername}%"`;
+    }
+
+    db.pool.query(query1, function(error, rows, fields){
+
+        let players = rows;
+
+        return res.render('positionPlayers', {data: players})
+})});
+
 
 module.exports = router;
