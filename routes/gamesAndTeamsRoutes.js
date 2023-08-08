04319b1a9413444d7dd92a5a5f094a7c6e5be4ef
Felix <fpidlaoan@gmail.com>
2023-08-07 21:21:10 -1000

    When adding games to the games table, now adds the games and teams to the games-table intersection table

--- routes/gamesAndTeamsRoutes.js
+++ routes/gamesAndTeamsRoutes.js
@@ -2,5 +2,27 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database/db-connector');
 
+router.get('/', (req, res) => {
+    let query1;
+
+    if (req.query.idgame === undefined)
+    {
+        query1 = `
+                SELECT
+                    games_idgame as game_id,
+                    teams_teamname as team_name
+                FROM games_has_teams;`;
+    }
+
+    // Run the 1st query
+    db.pool.query(query1, function(error, rows, fields){
+        
+        // Save the people
+        let gamesTeams = rows;
+
+        return res.render('gamesAndTeams', {data: gamesTeams});
+        
+    })
+});
 
 module.exports = router;
--- routes/gamesRoutes.js
+++ routes/gamesRoutes.js
@@ -18,8 +18,8 @@ router.get('/', (req, res) => {
         query1 = `SELECT * FROM games WHERE idgame LIKE "${req.query.idgame}%";`
     }
 
-    // Query 2 is the same in both cases
     let query2 = "SELECT * FROM teams;";
+
 
     // Run the 1st query
     db.pool.query(query1, function(error, rows, fields){
@@ -27,8 +27,14 @@ router.get('/', (req, res) => {
         // Save the people
         let games = rows;
 
-        return res.render('games', {data: games, games: games});
-        
+        // Run the second query
+        db.pool.query(query2, (error, rows, fields) => {
+    
+            // Save the teams
+            let teams = rows;
+
+            return res.render('games', {data: games, teams: teams});
+        })
     })
 });
 
@@ -39,19 +45,18 @@ router.post('/add', function(req, res){
 
     // Capture NULL values
     let winningscore = parseInt(data['input-winningscore']);
-    if (isNaN(winningscore))
-    {
-        winningscore = 'NULL'
-    }
+    winningscore = isNaN(winningscore) ? 'NULL' : winningscore;
 
     let losingscore = parseInt(data['input-losingscore']);
-    if (isNaN(losingscore))
-    {
-        losingscore = 'NULL'
-    }
+    losingscore = isNaN(losingscore) ? 'NULL' : losingscore;
 
-    // Create the query and run it on the database
-    query1 = `INSERT INTO games (winningteam, losingteam, winningscore, losingscore, location) VALUES ('${data['input-winningteam']}', '${data['input-losingteam']}', '${data['input-winningscore']}', '${data['input-losingscore']}', '${data['input-location']}'  )`;
+    // Construct first query
+    query1 =    `INSERT INTO games (winningteam, losingteam, winningscore, losingscore, location) VALUES 
+                ((SELECT teamname FROM teams WHERE teamname='${data['input-winningteam']}'), 
+                (SELECT teamname FROM teams WHERE teamname='${data['input-losingteam']}'), 
+                '${data['input-winningscore']}', '${data['input-losingscore']}', '${data['input-location']}')`;
+
+    // Execute first query
     db.pool.query(query1, function(error, rows, fields){
 
         // Check to see if there was an error
@@ -65,11 +70,49 @@ router.post('/add', function(req, res){
         // presents it on the screen
         else
         {
-            res.redirect('/games');
-        }
-    })
+
+            // Find the game we inserted and get the game id
+            query2 = `
+                    SELECT idgame
+                    FROM games
+                    WHERE (winningteam = '${data['input-winningteam']}' AND losingteam = '${data['input-losingteam']}' AND winningscore = '${data['input-winningscore']}' AND losingscore = '${data['input-losingscore']}')`;
+
+            db.pool.query(query2, function(error, rows, field) {
+                // Add games and teams to intersection table
+                let games = rows
+                let gamesID = games[0].idgame
+
+                if (error) {
+                    console.log(error)
+                    res.sendStatus(400)
+                
+                } else {
+
+                    // Add winning team
+                    query3 = `INSERT INTO games_has_teams ( games_idgame, teams_teamname ) VALUES 
+                    ('${gamesID}', (SELECT teamname FROM teams WHERE teamname='${data['input-winningteam']}' ) )`
+
+                    db.pool.query(query3, function(error, rows, fields) {
+                        if (error) {
+                            console.log(error)
+                            res.sendStatus(400);
+
+                        } else {
+                            // Add losing team
+                            query4 = `INSERT INTO games_has_teams ( games_idgame, teams_teamname ) VALUES 
+
+                            ('${gamesID}', (SELECT teamname FROM teams WHERE teamname='${data['input-losingteam']}' ) )`
+                            db.pool.query(query4, function(error, rows, fields) {
+                                if (error) {
+                                    console.log(error);
+                                    res.sendStatus(400);
+                                } else {
+                                    res.redirect('/games')
+                            }});
+                    }});
+            }});
+    }})
 });
-;
 
 
 router.put('/update', function(req,res,next){
@@ -108,8 +151,7 @@ router.put('/update', function(req,res,next){
                         }
                     })
                 }
-    
-    })});
+})});
 
 
 
